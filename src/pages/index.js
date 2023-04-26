bceb02fb3c252c43373c7e7ae3902e18246fd00d
teriTheDesigner <tere1109@stud.kea.dk>
2023-04-26 15:58:29 +0200

    Form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "./Home.module.css";
+import { estateTypes } from "../data/estateTypes";
 
 export default function Home() {
   return (
@@ -33,10 +34,28 @@ export default function Home() {
           </p>
           <form action="/buyers" method="GET" className={styles.form}>
             <label>
+              <span className={styles.label}>Price</span>
+              <input name="price" required />
+            </label>
+            <label>
+              <span className={styles.label}>Size</span>
+              <input name="size" required />
+            </label>
+            <label>
               <span className={styles.label}>Zip Code</span>
               <input name="zipCode" required />
             </label>
-            <button className={styles.button}>Submit</button>
+            <label>
+              <span className={styles.label}>Estate type</span>
+              <select name="estate_type" required>
+                {estateTypes.map(({ name, id }) => (
+                  <option key={id} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button className={styles.button}>Find potential buyers</button>
           </form>
         </div>
       </div>
