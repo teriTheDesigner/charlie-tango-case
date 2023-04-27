350524990c9b13db6e26f5251d69ebb7cf72e4b5
teriTheDesigner <tere1109@stud.kea.dk>
2023-04-27 11:24:36 +0200

    second page

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -6,21 +6,16 @@ import { useState, useEffect } from "react";
 export default function Buyers() {
   const { query } = useRouter();
   const [buyers, setBuyers] = useState([]);
-  console.log(query.zipCode);
-  console.log(query.estate_type);
-  console.log(query.price);
-  console.log(query.size);
 
   useEffect(() => {
     // Check if zipCode exists in the query
-    if (query.zipCode) {
-      fetch(
-        `/api/find-buyers?price=${query.price}&size=${query.size}&zipCode=${query.zipCode}&estate_type=${query.estate_type}`
-      )
-        .then((res) => res.json())
-        .then((data) => setBuyers(data));
-    }
-  }, [query.zipCode, query.estate_type, query.price, query.size]);
+
+    fetch(
+      `/api/find-buyers?price=${query.price}&size=${query.size}&zipCode=${query.zipCode}&estate_type=${query.estate_type}`
+    )
+      .then((res) => res.json())
+      .then((data) => setBuyers(data));
+  }, [query]);
 
   console.log({ buyers });
   return (
@@ -30,51 +25,57 @@ export default function Buyers() {
       </Head>
       <div className={`wrapper ${styles.cardPageLayout}`}>
         <h1 className={styles.headline}>Potential buyers</h1>
-
-        {buyers.map((buyer) => (
-          <div key={buyer.id} className={styles.card}>
-            <input type="checkbox" className={styles.checkbox}></input>
-            <div className={styles.card_content}>
-              <p>Buyer's ID</p>
-              <p>{buyer.id}</p>
+        <form action="/contact" method="GET">
+          {buyers.map((buyer) => (
+            <div key={buyer.id} className={styles.card}>
+              <input
+                type="checkbox"
+                name="id[]"
+                value={buyer.id}
+                className={styles.checkbox}
+              ></input>
+              <div className={styles.card_content}>
+                <p>Buyer's ID</p>
+                <p>{buyer.id}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Estate Type</p>
+                <p>{buyer.estateType}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Takeover Date</p>
+                <p>{buyer.takeoverDate}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Adults</p>
+                <p>{buyer.adults}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Children</p>
+                <p>{buyer.children}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Min Size</p>
+                <p>{buyer.minSize}</p>
+              </div>
+              <div className={styles.card_content}>
+                <p>Max Price</p>
+                <p>{buyer.maxPrice}</p>
+              </div>
+              <div>
+                <p>Description</p>
+                <p>{buyer.description}</p>
+              </div>
             </div>
-            <div className={styles.card_content}>
-              <p>Estate Type</p>
-              <p>{buyer.estateType}</p>
-            </div>
-            <div className={styles.card_content}>
-              <p>Takeover Date</p>
-              <p>{buyer.takeoverDate}</p>
-            </div>
-            <div className={styles.card_content}>
-              <p>Adults</p>
-              <p>{buyer.adults}</p>
-            </div>
-            <div className={styles.card_content}>
-              <p>Children</p>
-              <p>{buyer.children}</p>
-            </div>
-            <div className={styles.card_content}>
-              <p>Min Size</p>
-              <p>{buyer.minSize}</p>
-            </div>
-            <div className={styles.card_content}>
-              <p>Max Price</p>
-              <p>{buyer.maxPrice}</p>
-            </div>
-            <div>
-              <p>Description</p>
-              <p>{buyer.description}</p>
-            </div>
-          </div>
-        ))}
-
-        {/*  <div className={styles.content}>
+          ))}
+          <button>Page 3</button>
+        </form>
+        <div className={styles.content}>
           <h2>Query params:</h2>
           <pre>
             <code>{JSON.stringify(query, null, 2)}</code>
           </pre>
-        </div> */}
+        </div>
       </div>
     </>
   );
