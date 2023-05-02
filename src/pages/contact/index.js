3ce9a3ad941b2a599250e8e59c277026a919ab69
Robert Stoica <robe3750@stud.kea.dk>
2023-05-02 09:11:31 +0200

    contact_styling

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,7 +8,7 @@ export default function Contact() {
   const { query } = useRouter();
   function submitted(e) {
     e.preventDefault();
-    console.log({ buyers });
+    // console.log({ buyers });
     const payload = {
       name: e.target.name.value,
       email: e.target.email.value,
@@ -27,10 +27,10 @@ export default function Contact() {
       body: JSON.stringify(payload),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log("data is", data));
   }
 
-  console.log({ query });
+  // console.log({ query });
 
   const [buyers, setBuyers] = useState([]);
   useEffect(() => {
@@ -40,6 +40,7 @@ export default function Contact() {
   }, [query.id]);
 
   console.log("buyers:", buyers);
+
   function deleteBuyer(id) {
     setBuyers((prev) => prev.filter((buyer) => buyer.id !== id));
   }
@@ -53,40 +54,90 @@ export default function Contact() {
         <h1 className={styles.headline}>Potential buyers</h1>
         <div className={styles.content}>
           <h2>Buyers</h2>
-          <ul>
-            {buyers.map((buyer) => (
-              <li key={buyer.id}>
-                <button onClick={() => deleteBuyer(buyer.id)}>X</button>
-                <pre>
-                  <code>{JSON.stringify(buyer, null, 2)}</code>
-                </pre>
+          <div className={"page-section"}>
+            <ul className={"card_wrapper"}>
+              {/* <li> */}
+              {/* {buyers.map((buyer) => (
+              // <li key={buyer.id} className={styles.buyerItem}>
+              //   <button onClick={() => deleteBuyer(buyer.id)}>X</button>
+              //   <pre className={styles.buyerContent}>
+              //     <code>{JSON.stringify(buyer, null, 2)}</code>
+              //   </pre>
+              // </li>
+              //_______________________________1__
+              <li key={buyer.id} className={styles.buyerItem}>
+                <div>
+                  <p className={styles.buyerID}>ID: {buyer.id}</p>
+                  <p className={styles.buyerEmail}>Email: {buyer.email}</p>
+                </div>
               </li>
-            ))}
-          </ul>
-          <form
-            className={styles.contactForm}
-            ref={formEl}
-            onSubmit={submitted}
-          >
-            <label>
-              Name
-              <input type="text" name="name" required />
-            </label>
-            <label>
-              Email Address
-              <input type="email" name="email" required />
-            </label>
-            <label>
-              Phone Number
-              <input type="tel" name="phone" required />
-            </label>
-            <label>
-              <input type="checkbox" name="offersAndInfo" />
-              Yes please, EDC may contact me with offers and information related
-              to the real estate market.
-            </label>
-            <button type="submit">Contact buyers</button>
-          </form>
+              //_______________________________1__
+            ))} */}
+
+              {buyers.map((buyer) => (
+                <div key={buyer.id} className={styles.card}>
+                  <button onClick={() => deleteBuyer(buyer.id)}>X</button>
+                  <div className={styles.card_content}>
+                    <p>Buyer&apos;s ID</p>
+                    <p>{buyer.id}</p>
+                  </div>
+                  {/* <div className={styles.card_content}>
+                  <p>Estate Type</p>
+                  <p>{buyer.estateType}</p>
+                </div>
+                <div className={styles.card_content}>
+                  <p>Takeover Date</p>
+                  <p>{buyer.takeoverDate}</p>
+                </div>
+                <div className={styles.card_content}>
+                  <p>Adults</p>
+                  <p>{buyer.adults}</p>
+                </div>
+                <div className={styles.card_content}>
+                  <p>Children</p>
+                  <p>{buyer.children}</p>
+                </div>
+                <div className={styles.card_content}>
+                  <p className={styles.icon}>Min Size</p>
+                  <p>{buyer.minSize}</p>
+                </div>
+                <div className={styles.card_content}>
+                  <p>Max Price</p>
+                  <p>{buyer.maxPrice}</p>
+                </div> */}
+                  <div>
+                    <p>Description</p>
+                    <p>{buyer.description}</p>
+                  </div>
+                </div>
+              ))}
+            </ul>
+
+            <form
+              className={styles.contactForm}
+              ref={formEl}
+              onSubmit={submitted}
+            >
+              <label>
+                Name
+                <input type="text" name="name" required />
+              </label>
+              <label>
+                Email Address
+                <input type="email" name="email" required />
+              </label>
+              <label>
+                Phone Number
+                <input type="tel" name="phone" required />
+              </label>
+              <label>
+                <input type="checkbox" name="offersAndInfo" />
+                Yes please, EDC may contact me with offers and information
+                related to the real estate market.
+              </label>
+              <button type="submit">Contact buyers</button>
+            </form>
+          </div>
         </div>
       </div>
     </>
