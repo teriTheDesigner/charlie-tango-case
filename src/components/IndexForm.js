8fc2a758c4ce6660bcda9f36711b9275679b585a
Robert Stoica <robe3750@stud.kea.dk>
2023-05-03 20:56:09 +0200

    reload issue

--- src/components/IndexForm.js
+++ src/components/IndexForm.js
@@ -1,9 +1,30 @@
 import styles from "@/pages/Home.module.css";
 import { estateTypes } from "@/data/estateTypes";
 
+import { useRouter } from "next/router";
+
 export default function IndexForm() {
+  const router = useRouter();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const query = {
+      price: formData.get("price"),
+      size: formData.get("size"),
+      zipCode: formData.get("zipCode"),
+      estateType: formData.get("estateType"),
+    };
+
+    router.push({
+      pathname: "/buyers",
+      query,
+    });
+  };
+
   return (
-    <form action="/buyers" method="GET" className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <label>
         <span className={styles.label}>Price</span>
         <input name="price" required />
