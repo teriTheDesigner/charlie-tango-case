f3aa55f109344814d1ce0c7587cd43813a781b3c
teriTheDesigner <tere1109@stud.kea.dk>
2023-05-06 17:03:12 +0200

    fixed_thank_you

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -36,35 +36,51 @@ export default function Sellers({ sellers }) {
             <h2>Seller ID: {seller.id}</h2>
 
             <div className={styles.oneField}>
-              <p>Id</p>
+              <p>
+                <b>Id</b>
+              </p>
               <p>{seller.id}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Name</p>
+              <p>
+                <b>Name</b>
+              </p>
               <p>{seller.name}</p>
             </div>
             <div className={styles.oneField}>
-              <p>E-mail</p>
+              <p>
+                <b>E-mail</b>
+              </p>
               <p>{seller.email}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Phone</p>
+              <p>
+                <b>Phone</b>
+              </p>
               <p>{seller.phone}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Price</p>
+              <p>
+                <b>Price</b>
+              </p>
               <p>{seller.price}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Zip Code</p>
+              <p>
+                <b>Zip Code</b>
+              </p>
               <p>{seller.zipCode}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Size</p>
+              <p>
+                <b>Size</b>
+              </p>
               <p>{seller.size}</p>
             </div>
             <div className={styles.oneField}>
-              <p>Estate Type</p>
+              <p>
+                <b>Estate Type</b>
+              </p>
               <p>{seller.estateType}</p>
             </div>
           </div>
@@ -73,23 +89,33 @@ export default function Sellers({ sellers }) {
             {seller.buyers.map((buyer) => (
               <div key={buyer.id} className={styles.oneBuyer}>
                 <div className={styles.oneField}>
-                  <p>Buyer ID</p>
+                  <p>
+                    <b>Buyer ID</b>
+                  </p>
                   <p>{buyer.id}</p>
                 </div>
                 <div className={styles.oneField}>
-                  <p>Max Price</p>
+                  <p>
+                    <b>Max Price</b>
+                  </p>
                   <p>{buyer.maxPrice}</p>
                 </div>
                 <div className={styles.oneField}>
-                  <p>Max Size</p>
+                  <p>
+                    <b>Max Size</b>
+                  </p>
                   <p>{buyer.minSize}</p>
                 </div>
                 <div className={styles.oneField}>
-                  <p>Adults</p>
+                  <p>
+                    <b>Adults</b>
+                  </p>
                   <p>{buyer.adults}</p>
                 </div>
                 <div className={styles.oneField}>
-                  <p>Children</p>
+                  <p>
+                    <b>Children</b>
+                  </p>
                   <p>{buyer.children}</p>
                 </div>
               </div>
--- src/pages/thanks/index.js
+++ src/pages/thanks/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../Home.module.css";
+import style from "./Thanks.module.css";
 import { useRouter } from "next/router";
 
 export default function Thanks() {
@@ -14,10 +15,22 @@ export default function Thanks() {
       <Head>
         <title>Thank you | EDC</title>
       </Head>
-      <h1 className={styles.headline}>Thank you!</h1>
-      <button className={styles.button} onClick={handleViewDashboard}>
-        View dashboard
-      </button>
+      <div className={style.layout}>
+        <h1 className={styles.headline}>Thank you!</h1>
+        <img className={style.img} alt="icon"></img>
+        <div className={style.div}>
+          <p>
+            Thank you for registering your property with our agency! Our team of
+            experienced agents will review your information and contact you as
+            soon as possible to discuss the next steps in the selling process.
+            If you have any questions or concerns, please don't hesitate to
+            reach out to us.
+          </p>
+          <button className={styles.button} onClick={handleViewDashboard}>
+            View dashboard
+          </button>
+        </div>
+      </div>
     </>
   );
 }
